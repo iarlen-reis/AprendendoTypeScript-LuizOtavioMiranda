4a745052b0fa13e6f9acdd6eac03e845de191ca2
= <iarlensr@hotmail.com>
2023-02-27 18:05:30 -0300

    aprendendo a criar tupla com typescript.

---
+++ 1.TiposBasicos/6.TipoTuple/tipoTuple.js
@@ -0,0 +1,8 @@
+/*
+Aprendendo tiplagem de tupla:
+- É um tipo exclusivo do typescript.
+
+- É um array com tipos e tamanho especifico.
+*/
+var dataClient = [1, "Iarlen"];
+console.log(dataClient);
